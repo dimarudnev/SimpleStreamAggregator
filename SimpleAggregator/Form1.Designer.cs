d63c82f5a7517abcfcb44efd43bac6f8145486a8
dimarudnev <dima__rudnev@mail.ru>
2016-05-12 23:30:28 +0300

    First commit

---
+++ SimpleAggregator/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SimpleAggregator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 153);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Time frame lenght:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 46);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(123, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "10";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(89, 98);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(123, 20);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "result";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 101);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "File postfix:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(89, 72);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(123, 20);
+            this.textBox5.TabIndex = 22;
+            this.textBox5.Text = "10";
+            //
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Frames count:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 210);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SimpleAggregator/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SimpleAggregator {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+
+            if(dr == DialogResult.OK) {
+                textBox1.Text = this.openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if(!backgroundWorker1.IsBusy) {
+                backgroundWorker1.RunWorkerAsync(new CalculatorOptions {
+                    SourcePath = @"D:\Data\LANL\source",
+                    DestinationPath = @"D:\Data\LANL\",
+                    TimeFrame = Convert.ToInt32(this.textBox4.Text),
+                    FrameCount = Convert.ToInt32(this.textBox5.Text),
+                    FilePostfix = this.textBox6.Text
+                });
+            }
+        }
+        private void button3_Click(object sender, EventArgs e) {
+            if(backgroundWorker1.IsBusy) {
+                label4.Text = "Cancelling...";
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+            var options = (CalculatorOptions)e.Argument;
+            BackgroundWorker bw = sender as BackgroundWorker;
+            var calc = new Aggregator(bw, options);
+
+
+            var readers = new List<ReaderBase> {
+                new ProcReader(options, calc),
+                new DnsReader(options, calc),
+                new FlowsReader(options, calc)
+            };
+            for(int i = 0; i < options.FrameCount; i++) {
+                calc.Begin();
+                List<Task> tasks = new List<Task>();
+                foreach(ReaderBase reader in readers) {
+                    tasks.Add(Task.Factory.StartNew(() => reader.ReadNextTimeStamp(i)));
+                }
+                Task.WaitAll(tasks.ToArray());
+                bw.ReportProgress((int)(((i + 1) * 100) / options.FrameCount));
+                calc.End(i);
+            }
+            var resultFile = options.DestinationPath + options.FilePostfix;
+            using(var fileStream = new FileStream(resultFile, FileMode.Create)) {
+                using(var streamWriter = new StreamWriter(fileStream)) {
+                    calc.WriteResult(streamWriter);
+                }
+            }
+            e.Cancel = calc.Cancelled;
+            e.Result = calc.Result;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if(e.Cancelled) {
+                this.progressBar1.Value = 0;
+                this.label4.Text = "Cancelled";
+            } else {
+                this.label4.Text = String.Format("Ready! Line count: {0}.", e.Result.ToString());
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            this.progressBar1.Value = e.ProgressPercentage;
+            if(e.UserState != null) {
+                this.label4.Text = e.UserState.ToString();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+    class CalculatorOptions {
+        internal string FilePostfix;
+
+        public string SourcePath { get; set; }
+        public int ColIndex { get; set; }
+        public int RowIndex { get; set; }
+        public int TimeFrame { get; set; }
+        public int FrameCount { get; set; }
+        public string DestinationPath { get; internal set; }
+    }
+    class Aggregator : CalculatorBase {
+        List<EventsInfo> basis = null;
+        List<Aggregation> aggregations = new List<Aggregation>();
+
+        Dictionary<string, EventsInfo> current = new Dictionary<string, EventsInfo>();
+
+        public Aggregator(BackgroundWorker worker, CalculatorOptions options) : base(worker, options) {
+            
+        }
+
+        public override void Begin() {
+            current.Clear();
+        }
+        public override void End(int timeIndex) {
+            if(basis == null) {
+                basis = new List<EventsInfo>() {
+                    current.Values.ElementAt(0),
+                    current.Values.ElementAt(100)
+                };
+            }
+            int[,] matrix = new int[current.Keys.Count, basis.Count];
+            int i = 0, j = 0;
+            foreach(KeyValuePair<string, EventsInfo> item in current) {
+                foreach(EventsInfo basisItem in basis) {
+                    matrix[i, j++] = EventsInfo.CalcDictance(item.Value, basisItem);
+                }
+                i++;
+                j = 0;
+            }
+            aggregations.Add(new Aggregation() {
+                Index = timeIndex,
+                Matrix = matrix,
+                RowNames = current.Keys.ToArray()
+            });
+            current.Clear();
+        }
+        public override void WriteResult(StreamWriter writer) {
+            foreach(Aggregation info in aggregations) {
+                //writer.WriteLine("========= Time: {0} ({1} - {2})==========", info.Key, info.Value.StartTime, info.Value.EndTime);
+                info.WriteToFile(writer, worker);
+            }
+            
+        }
+
+        static object locker = new { };
+        public override void AddValue(string rowName, string[] colNames) {
+            lock (locker) {
+                foreach(var colName in colNames) {
+                    EventsInfo row;
+                    if(!current.TryGetValue(rowName, out row)) {
+                        row = new EventsInfo();
+                        current.Add(rowName, row);
+                    }
+                    if(row.ContainsKey(colName)) {
+                        row[colName]++;
+                    } else {
+                        row.Add(colName, 1);
+                    }
+                }
+            }
+        }
+    }
+    class Aggregation  {
+        public int Index { get; set; }
+        public string[] RowNames { get; set; }
+        public int[,] Matrix { get; set; }
+
+        public void WriteToFile(StreamWriter writer, BackgroundWorker worker) {
+            var dim1 = Matrix.GetUpperBound(0) + 1;
+            var dim2 = Matrix.GetUpperBound(1) + 1;
+            for(int i = 0; i < dim1; i++) {
+                writer.Write(string.Format("{0},", Index));
+                writer.Write(string.Format("{0},", RowNames[i]));
+                for(int j = 0; j < dim2; j++) {
+                    if(j != 0)
+                        writer.Write(",");
+                    writer.Write(Matrix[i, j]);
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+    class EventsInfo : Dictionary<string, int> {
+        public static int CalcDictance(EventsInfo value1, EventsInfo value2) {
+            var distance = 0;
+            var intersectKeys = value1.Keys.Intersect(value2.Keys);
+            var uniqueKeysForValue1 = value1.Keys.Except(intersectKeys);
+            var uniqueKeysForValue2 = value2.Keys.Except(intersectKeys);
+            foreach(string key in intersectKeys) {
+                distance += (int)Math.Pow(value1[key] - value2[key], 2);
+            }
+            foreach(string key in uniqueKeysForValue1) {
+                distance += (int)Math.Pow(value1[key], 2);
+            }
+            foreach(string key in uniqueKeysForValue2) {
+                distance += (int)Math.Pow(value2[key], 2);
+            }
+            return (int)Math.Sqrt(distance);
+        }
+    }
+    
+    class CalculatorBase {
+        protected CalculatorOptions options;
+        protected BackgroundWorker worker;
+        int lineCount = 0;
+        
+
+        public bool Cancelled { get { return worker.CancellationPending; } }
+        public object Result { get { return lineCount; } }
+
+        public CalculatorBase(BackgroundWorker worker, CalculatorOptions options) {
+            this.worker = worker;
+            this.options = options;
+        }
+        void ReportProgress(Stream stream) {
+            worker.ReportProgress((int)((100 * stream.Position) / stream.Length));
+        }
+        
+        public virtual void WriteResult(StreamWriter writer) {
+        }
+
+        public virtual void Begin() {
+
+        }
+        public virtual void End(int timeIndex) {
+
+        }
+
+
+        public virtual void AddValue(string rowName, string[] colNames) { 
+        }
+    }
+
+    abstract class ReaderBase {
+        CalculatorBase calculator;
+        FileStream fileStream;
+        StreamReader streamReader;
+        CalculatorOptions options;
+
+        protected abstract string FileName { get; }
+
+        public ReaderBase(CalculatorOptions options, CalculatorBase calculator) {
+            this.options = options;
+            this.calculator = calculator;
+            fileStream = new FileStream(Path.Combine(options.SourcePath, FileName), FileMode.Open);
+            streamReader = new StreamReader(fileStream);
+
+        }
+
+        protected virtual int GetTimeStamp(string[] lineParts) {
+            return Convert.ToInt32(lineParts[0]);
+        }
+        protected abstract string GetRowValue(string[] lineParts);
+        protected abstract string[] GetColumnValues(string[] lineParts);
+
+        public void ReadNextTimeStamp(int timeIndex) {
+            while(!streamReader.EndOfStream) {
+                string line = streamReader.ReadLine();
+                string[] lineParts = line.Split(',');
+                var timeStamp = GetTimeStamp(lineParts);
+                int currentTimeIndex = (int)(timeStamp / this.options.TimeFrame);
+                if(timeIndex == currentTimeIndex) {
+                    calculator.AddValue(GetRowValue(lineParts), GetColumnValues(lineParts));
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    class ProcReader : ReaderBase {
+        protected override string FileName { get { return "proc.txt"; } }
+
+        public ProcReader(CalculatorOptions options, CalculatorBase calculator): base(options, calculator) {
+
+        }
+
+        protected override string[] GetColumnValues(string[] lineParts) {
+            return new string[1] { lineParts[3] };
+        }
+
+        protected override string GetRowValue(string[] lineParts) {
+            return lineParts[2];
+        }
+    }
+    class DnsReader : ReaderBase {
+        protected override string FileName { get { return "dns.txt"; } }
+
+        public DnsReader(CalculatorOptions options, CalculatorBase calculator) : base(options, calculator) {
+
+        }
+
+        protected override string[] GetColumnValues(string[] lineParts) {
+            return new string[1] { "dns" + lineParts[2] };
+        }
+
+        protected override string GetRowValue(string[] lineParts) {
+            return lineParts[1];
+        }
+    }
+    class FlowsReader : ReaderBase {
+        protected override string FileName { get { return "flows.txt"; } }
+
+        public FlowsReader(CalculatorOptions options, CalculatorBase calculator) : base(options, calculator) {
+
+        }
+
+        protected override string[] GetColumnValues(string[] lineParts) {
+            return new string[1] { "flows" + lineParts[4] + lineParts[5] };
+        }
+
+        protected override string GetRowValue(string[] lineParts) {
+            return lineParts[2];
+        }
+    }
+
+}
